800a9ca7715c0e7246d03d3378c051801a93c26c
massa <zoumaniguimassa12@gmail.com>
2023-05-19 13:48:26 +0000

    Update

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import UserList from './components/UserList';
+import store from './store/store';
+import './App.css';
+
+const App = () => (
+  <Provider store={store}>
+    <UserList />
+  </Provider>
+);
+
+export default App;
